f20c2bf53824a3eabd07eef19225f4e2af253306
Jose <josemaurette1234@gmail.com>
2025-02-16 15:29:15 -0500

    Added Other to WorkoutHistory

--- src/app/components/WorkoutHistory.tsx
+++ src/app/components/WorkoutHistory.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { useEffect, useState } from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ArrowLeft } from "lucide-react";
 
 interface Set {
   weight: number;
@@ -24,22 +27,47 @@ export default function WorkoutHistory() {
   const [lowerWorkoutHistory, setLowerWorkoutHistory] = useState<
     WorkoutEntry[]
   >([]);
+  const [otherWorkoutHistory, setOtherWorkoutHistory] = useState<
+    WorkoutEntry[]
+  >([]);
 
   useEffect(() => {
     // Load workout history from localStorage
     const storedUpper = localStorage.getItem("upperWorkoutHistory");
     const storedLower = localStorage.getItem("lowerWorkoutHistory");
+    const storedOther = localStorage.getItem("otherWorkoutHistory");
 
     if (storedUpper)
       setUpperWorkoutHistory(JSON.parse(storedUpper) as WorkoutEntry[]);
     if (storedLower)
       setLowerWorkoutHistory(JSON.parse(storedLower) as WorkoutEntry[]);
+    if (storedOther)
+      setOtherWorkoutHistory(JSON.parse(storedOther) as WorkoutEntry[]);
   }, []);
 
-  const renderWorkout = (workoutHistory: WorkoutEntry[], title: string) => (
+  const clearHistory = (type: "upper" | "lower" | "other") => {
+    if (type === "upper") {
+      localStorage.removeItem("upperWorkoutHistory");
+      setUpperWorkoutHistory([]);
+    } else if (type === "lower") {
+      localStorage.removeItem("lowerWorkoutHistory");
+      setLowerWorkoutHistory([]);
+    }
+    if (type === "other") {
+      localStorage.removeItem("otherWorkoutHistory");
+      setOtherWorkoutHistory([]);
+    }
+  };
+
+  const renderWorkout = (
+    workoutHistory: WorkoutEntry[],
+    title: string,
+    type: "upper" | "lower" | "other"
+  ) => (
     <Card className="mb-4">
-      <CardHeader>
+      <CardHeader className="flex items-center justify-between">
         <CardTitle className="text-2xl font-bold">{title}</CardTitle>
+        <Button onClick={() => clearHistory(type)}>Clear History</Button>
       </CardHeader>
       <CardContent>
         {workoutHistory.length > 0 ? (
@@ -76,9 +104,15 @@ export default function WorkoutHistory() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">Workout History</h1>
-      {renderWorkout(upperWorkoutHistory, "Upper Workout History")}
-      {renderWorkout(lowerWorkoutHistory, "Lower Workout History")}
+      <h1 className="text-3xl font-bold mb-6 flex items-center">
+        <Link href={"/workouts"}>
+          <ArrowLeft className="mr-2" />
+        </Link>
+        Workout History
+      </h1>
+      {renderWorkout(upperWorkoutHistory, "Upper Workout History", "upper")}
+      {renderWorkout(lowerWorkoutHistory, "Lower Workout History", "lower")}
+      {renderWorkout(otherWorkoutHistory, "Other Workout History", "other")}
     </div>
   );
 }

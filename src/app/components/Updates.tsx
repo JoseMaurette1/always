64bd95dcf9e8a9525285a9677d39360987d3cd37
Jose <josemaurette1234@gmail.com>
2025-02-17 21:04:00 -0500

    Update Page Depencides

--- src/app/components/Updates.tsx
+++ src/app/components/Updates.tsx
@@ -1,5 +1,4 @@
-// src/app/components/Updates.tsx
-"use client"; // Mark as a Client Component
+"use client";
 
 import React from "react";
 import Feb16 from "./patchnotes/Feb16";
@@ -11,38 +10,40 @@ import { motion } from "framer-motion";
 
 const Updates = () => {
   return (
-    <motion.div
-      className="flex items-center justify-center min-h-screen"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5, ease: "easeInOut" }}
-    >
-      <div className="container mx-auto py-8">
-        <motion.h1
-          className="text-2xl md:text-3xl font-bold text-center mb-4"
-          initial={{ y: -20, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.5, ease: "easeInOut", delay: 0.2 }}
-        >
-          <Link href={"/"}>
-            <ArrowLeft /> Latest Updates
-          </Link>
-        </motion.h1>
-        <motion.div
-          initial={{ opacity: 0, scale: 0.9 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5, ease: "easeInOut", delay: 0.4 }}
-        >
-          <ScrollArea className="h-[70vh] md:h-[500px] w-full rounded-md border">
-            <div className="p-4">
-              <Feb17 />
-              <Feb16 />
-              {/* Add more patch note components here */}
-            </div>
-          </ScrollArea>
-        </motion.div>
-      </div>
-    </motion.div>
+    <>
+      <motion.div
+        className="flex items-center justify-center min-h-screen"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+      >
+        <div className="container mx-auto py-8">
+          <motion.h1
+            className="text-2xl md:text-3xl font-bold text-center mb-4"
+            initial={{ y: -20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5, ease: "easeInOut", delay: 0.2 }}
+          >
+            <Link href={"/"}>
+              <ArrowLeft /> Latest Updates
+            </Link>
+          </motion.h1>
+          <motion.div
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5, ease: "easeInOut", delay: 0.4 }}
+          >
+            <ScrollArea className="h-[70vh] md:h-[500px] w-full rounded-md border">
+              <div className="p-4">
+                <Feb17 />
+                <Feb16 />
+                {/* Add more patch note components here */}
+              </div>
+            </ScrollArea>
+          </motion.div>
+        </div>
+      </motion.div>
+    </>
   );
 };
 

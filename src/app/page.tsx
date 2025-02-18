31e119ce2ce9d01edd467bdc2c8200bfafb4f78e
Jose <josemaurette1234@gmail.com>
2025-02-17 21:03:17 -0500

    Removed Junk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 "use client";
-import Navbar from "./components/Navbar";
 import LoginPage from "./components/LoginPage";
 export default function Home() {
   return (
     <>
-      <Navbar />
       <LoginPage />
     </>
   );

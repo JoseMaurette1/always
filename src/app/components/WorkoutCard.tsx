bb01a704c919864ce73fbab25015fdb9b5c4a860
Jose <josemaurette1234@gmail.com>
2025-02-23 18:02:40 -0500

    removed unnecesary house icon

--- src/app/components/WorkoutCard.tsx
+++ src/app/components/WorkoutCard.tsx
@@ -2,7 +2,6 @@
 "use client";
 
 import React from "react";
-import { House } from "lucide-react";
 import Link from "next/link";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import WorkoutExercises from "./WorkoutExercises";
@@ -54,9 +53,6 @@ const WorkoutCard: React.FC<WorkoutCardProps> = ({
             : workoutType === "lower"
             ? "Lower Workout"
             : "Other Workout"}
-          <Link href={"/"} className="flex items-center pr-2">
-            <House />
-          </Link>
         </CardTitle>
       </CardHeader>
       <CardContent>
